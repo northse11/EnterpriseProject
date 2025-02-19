63f100cb3f651aff45a29f71b7b9166a7afd29d6
Steven North <northse@mail.uc.edu>
2025-02-19 14:29:26 -0500

    Created the project in intellij idea

---
+++ UFC Tracker/src/main/java/com/ufcfightertracker/enterprise/UfcTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ufcfightertracker.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfcTrackerApplication.class, args);
+    }
+
+}
---
+++ UFC Tracker/src/test/java/com/ufcfightertracker/enterprise/UfcTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcfightertracker.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
